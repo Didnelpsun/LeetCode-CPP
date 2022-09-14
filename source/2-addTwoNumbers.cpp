6a9b5555f20557f8e969d812e194e90b3cc37b58
Didnelpsun <2675350965@qq.com>
2022-09-14 23:52:53 +0800

    更新

--- source/2-addTwoNumbers.cpp
+++ source/2-addTwoNumbers.cpp
@@ -1,3 +1,19 @@
 // 给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
 // 请你将两个数相加，并以相同形式返回一个表示和的链表。
 // 你可以假设除了数字0之外，这两个数都不会以0开头。
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    }
+};
---
+++ test/2-addTwoNumbers-test.cpp
@@ -0,0 +1,9 @@
+#include "../source/2-addTwoNumbers.cpp"
+
+using namespace std;
+
+void test_2() {
+    int nums[4] = {2, 7, 11, 15};
+    int target = 9;
+//    twoSum(nums, target);
+}
